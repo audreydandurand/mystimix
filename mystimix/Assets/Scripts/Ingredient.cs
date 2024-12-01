b798d7262002d060bf9a2050f27e984f795769dc
RandomizedRaptor <i.mseriousstuff@gmail.com>
2024-12-01 14:53:23 -0500

    les recettes fonctionnent!

--- mystimix/Assets/Scripts/Ingredient.cs
+++ mystimix/Assets/Scripts/Ingredient.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
-public class Ingredient : MonoBehaviour
-{
+public class Ingredient : MonoBehaviour {
     public int id;
-}
+    public Vector3 originalPosition; // Store the original position here
 
+    private void Start() {
+        originalPosition = transform.position;
+    }
+}
